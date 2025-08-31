aa1f23f5a0587cf92fcf540601ea47f2a6f7d773
Jaival Dharaiya <jaivalofficial1@gmail.com>
2025-08-31 08:10:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import json
 
 # --- Configuration ---
 DATABASE_NAME = 'mydatabase.db'
-# GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-GOOGLE_API_KEY = 'AIzaSyCkytADPt1mTv7zVkWjVn9tSaOQHA0usOQ'
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
 
 # Gemini API endpoint
 MODEL_ENDPOINT = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent?key={GOOGLE_API_KEY}"
